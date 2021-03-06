58e60489812816ab4a78ad17f7b64eaa944607f5
landersoft <landersoft@gmail.com>
2021-03-06 10:11:14 -0300

    se agregan clases y administracion

--- disec/Empleados/admin.py
+++ disec/Empleados/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from Empleados.models import Empleado,Falta_Atraso,Felicitaciones
 
+class AdminEmpleado(admin.ModelAdmin):
+    list_filter = ["rut"]
+    search_fields = ["rut"]
+    class Meta:
+        model = Empleado
 # Register your models here.
+
+
+class AdminFalta_Atraso(admin.ModelAdmin):
+    list_filter = ["empleado"]
+    search_fields = ["empleado"]
+    class Meta:
+        model = Falta_Atraso
+
+
+class AdminFelicitaciones(admin.ModelAdmin):
+    list_filter = ["empleado"]
+    search_fields = ["empleado"]
+    class Meta:
+        model = Felicitaciones
+
+
+admin.site.register(Empleado,AdminEmpleado)
+admin.site.register(Falta_Atraso, AdminFalta_Atraso)
+admin.site.register(Felicitaciones, AdminFelicitaciones)
+
+admin.site.site_header = 'Direccion de seguridad ciudadana, Inspección y Emergencia Comunal'
--- disec/Empleados/models.py
+++ disec/Empleados/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+class Empleado(models.Model):
+
+    CONTRATA = 'contrata'
+    INDEFINIDO = 'indefinido'
+    HONORARIOS = 'honorarios'
+
+    TIPO = [
+        (CONTRATA, 'contrata'),
+        (INDEFINIDO, 'indefinido'),
+        (HONORARIOS, 'honorarios'),
+    ]
+
+    rut = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    domicilio = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=9)
+    licencia_conducir = models.CharField(max_length=1)
+
+    tipo_contrato = models.CharField(max_length=10, choices=TIPO)
+    fecha_contrata = models.DateField(blank=True)
+    vigencia = models.DateField(blank=True)
+    contacto_emergencia = models.CharField(max_length=100)
+    telefono_emergencia = models.CharField(max_length=9)
+    grupo_sanguineo = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return str(self.nombre)
+
+
+class Falta_Atraso(models.Model):
+    tipo = models.CharField(max_length=10)
+    fecha = models.DateField()
+    motivo = models.CharField(max_length=200)
+    empleado = models.ForeignKey('Empleado', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['empleado']
+        verbose_name = 'Falta y/o atraso'
+        verbose_name_plural = 'Faltas y/o atrasos'
+
+        def __str__(self):
+            return str(self.id)
+
+class Felicitaciones(models.Model):
+    tipo = models.CharField(max_length=10)
+    fecha = models.DateField()
+    motivo = models.CharField(max_length=200)
+    empleado = models.ForeignKey('Empleado', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['empleado']
+        verbose_name = 'Felicitaciones y Distincion'
+        verbose_name_plural = 'Felicitaciones y Distinciones'
+
+        def __str__(self):
+            return str(self.id)
+
+    
